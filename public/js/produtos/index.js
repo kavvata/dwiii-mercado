71cc0886a6202e33c9798b600b5e29b90b650a99
kavvata <gabriel.kawata@gmail.com>
2024-10-31 15:00:05 -0300

    adicionado anotação

--- public/js/produtos/index.js
+++ public/js/produtos/index.js
@@ -7,7 +7,9 @@ input.onkeyup = () => {
     // console.log(table_rows)
     let table_rows = document.querySelectorAll('table tr')
 
+    // NOTE: pulando o primeiro elemento pois seria o cabecalho da tabela
     for (let i = 1; i < table_rows.length; i++) {
+        // NOTE: Celula 0 seria o td com o nome do produto
         if (!table_rows[i].cells[0].innerHTML.toLowerCase().includes(filtro)) {
             table_rows[i].style.display = 'none'
         } else {

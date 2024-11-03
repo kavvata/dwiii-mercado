8f25ec2afc06e16aba6754c51169416c49fe7fd1
kavvata <gabriel.kawata@gmail.com>
2024-11-03 20:49:22 -0300

    Estilizando a tabela de produtos

--- public/js/produtos/index.js
+++ public/js/produtos/index.js
@@ -5,12 +5,12 @@ input.onkeyup = () => {
     let filtro = input.value.toLowerCase()
     // table_rows = document.getElementsByTagName('table')[0].getElementsByTagName('tr')
     // console.log(table_rows)
-    let table_rows = document.querySelectorAll('table tr')
+    let table_rows = document.querySelectorAll('#table-produto tr')
 
     // NOTE: pulando o primeiro elemento pois seria o cabecalho da tabela
     for (let i = 1; i < table_rows.length; i++) {
         // NOTE: Celula 0 seria o td com o nome do produto
-        if (!table_rows[i].cells[0].innerHTML.toLowerCase().includes(filtro)) {
+        if (!table_rows[i].cells[1].innerHTML.toLowerCase().includes(filtro)) {
             table_rows[i].style.display = 'none'
         } else {
             table_rows[i].style.display = 'revert'
--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -1,50 +1,53 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
             {{ __('Produtos') }}
         </h2>
     </x-slot>
     <!-- You must be the change you wish to see in the world. - Mahatma Gandhi -->
+
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="mx-auto max-w-5xl pb-12 sm:px-6 lg:px-8">
             <div
-                class="flex flex-col gap-6 overflow-hidden bg-white p-6 text-gray-800 shadow-sm sm:rounded-lg dark:bg-gray-800 dark:text-gray-200">
-                <div class="h-8 align-center flex justify-between">
-                    <input id="search" class="border border-slate-600 rounded-lg dark:bg-slate-900"
+                class="flex flex-col gap-6 overflow-hidden bg-white  p-6 text-gray-800 shadow-sm sm:rounded-lg dark:bg-gray-800 dark:text-gray-200">
+                <div class="align-center flex h-8 justify-between">
+                    <input id="search" class="rounded-lg border border-slate-600 dark:bg-slate-900"
                         placeholder="Procure um nome..." type="text">
-                    <a class="flex p-2 border border-gray-600 bg-slate-600 hover:bg-slate-700 rounded-md justify-items-center items-center"
+                    <a class="flex items-center justify-items-center rounded-md border border-gray-600 bg-slate-600 p-2 hover:bg-slate-700"
                         href="{{ route('produtos.create') }}">
                         Novo Produto
                     </a>
                 </div>
-                <table class="w-full table-fixed">
-                    <tr class="border border-slate-600">
-                        <th class="border border-slate-600">Nome</th>
-                        <th class="border border-slate-600">Quantidade</th>
-                        <th class="border border-slate-600">Preço</th>
-                        <th class="border border-slate-600">Ações</th>
+            </div>
+        </div>
+        <div class="mx-auto max-w-5xl sm:px-6 lg:px-8">
+            <div
+                class="overflow-hidden bg-white text-gray-800 shadow-sm sm:rounded-lg dark:bg-gray-800 dark:text-gray-200">
+                <table id="table-produto" class="w-full table-fixed overflow-hidden">
+                    <tr class="bg-gray-700">
+                        <th class="w-16 px-6 pb-2 text-start">Qntd.</th>
+                        <th class="px-6 pb-2 text-start">Nome</th>
+                        <th class="w-16 px-6 pb-2 text-start">Preço</th>
+                        <th class="px-6 pb-2">Ações</th>
                     </tr>
                     @foreach ($produtos as $produto)
-                        <tr class="border border-slate-600 dark:hover:bg-gray-900">
-                            <td class="border border-slate-600 py-2 text-center"> {{ $produto->nome }} </td>
-                            <td class="border border-slate-600 py-2 text-center"> {{ $produto->quantidade }} </td>
+                        <tr class=" border-b last:border-b-0 border-slate-600 dark:hover:bg-gray-900">
+                            <td class="px-6 py-2 dark:text-gray-400"> {{ $produto->quantidade }} </td>
+                            <td class="px-6 py-2"> {{ $produto->nome }} </td>
 
-                            <td class="border border-slate-600 py-2 text-center">
+                            <td class="px-6 py-2">
                                 R${{ number_format($produto->preco, 2, ',') }}
                             </td>
 
-                            <td class="flex lg:flex-row justify-center gap-2 py-2 flex-col items-center">
+                            <td class="flex flex-col items-center justify-center gap-4 px-6 py-2 lg:flex-row">
 
-                                <a class="border border-slate-600 px-2 hover:bg-slate-800 rounded-md"
-                                    href="{{ route('produtos.show', $produto) }}">Mostrar</a>
-
-                                <a class="border border-slate-600 px-2 hover:bg-slate-800 rounded-md"
+                                <a class="w-20 rounded-md border border-slate-600 px-2 text-center hover:bg-slate-800 shadow-lg"
                                     href="{{ route('produtos.edit', $produto) }}">Editar</a>
 
                                 <form method="POST" action="{{ route('produtos.destroy', $produto) }}">
                                     @method('DELETE')
                                     @csrf
-                                    <button class="bg-red-600 px-2 hover:bg-red-900 rounded-md "
+                                    <button class="w-20 rounded-md bg-red-600 px-2 hover:bg-red-900"
                                         type="">Remover</button>
                                 </form>
 

e598caf223fd2f400f90c825be944758e3bee48b
kavvata <gabriel.kawata@gmail.com>
2024-12-10 17:47:21 -0300

    Atualização de atributos no modelo UnidadeMedida

--- app/Models/UnidadeMedida.php
+++ app/Models/UnidadeMedida.php
@@ -3,10 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @mixin IdeHelperUnidadeMedida
+ */
 class UnidadeMedida extends Model
 {
     /** @use HasFactory<\Database\Factories\UnidadeMedidaFactory> */
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['sigla', 'descricao'];
+
+    public function produtos(): HasMany
+    {
+        return $this->hasMany(Produto::class);
+    }
 }

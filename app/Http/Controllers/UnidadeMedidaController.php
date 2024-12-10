ef03b017f6860cf12b72f760a2b8635e60f91f48
kavvata <gabriel.kawata@gmail.com>
2024-12-10 16:59:13 -0300

    Criação de scaffold de UnidadeMedida

---
+++ app/Http/Controllers/UnidadeMedidaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UnidadeMedida;
+use Illuminate\Http\Request;
+
+class UnidadeMedidaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UnidadeMedida $unidadeMedida)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UnidadeMedida $unidadeMedida)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, UnidadeMedida $unidadeMedida)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UnidadeMedida $unidadeMedida)
+    {
+        //
+    }
+}

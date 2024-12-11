1a2c667e1031e20958b3eee458f24236115eb87c
kavvata <gabriel.kawata@gmail.com>
2024-12-11 10:37:33 -0300

    Correção de layout de ações nas tabelas de listagem

--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -44,18 +44,18 @@
                             <td class="px-6 py-2"> {{ $categoria->nome }} </td>
                             <td class="px-6 py-2"> {{ $categoria->descricao }} </td>
 
-                            <td class="flex flex-col items-center justify-center gap-4 px-6 py-2 md:flex-row">
+                            <td class="px-6 py-2">
+                                <div class="flex flex-col items-center justify-center gap-4 md:flex-row">
+                                    <a class="w-20 rounded-md border border-slate-600 px-2 text-center hover:bg-slate-800 shadow-lg"
+                                        href="{{ route('categorias.edit', $categoria) }}">Detalhes</a>
 
-                                <a class="w-20 rounded-md border border-slate-600 px-2 text-center hover:bg-slate-800 shadow-lg"
-                                    href="{{ route('categorias.edit', $categoria) }}">Editar</a>
-
-                                <form method="POST" action="{{ route('categorias.destroy', $categoria) }}">
-                                    @method('DELETE')
-                                    @csrf
-                                    <button class="w-20 rounded-md bg-red-600 px-2 hover:bg-red-900"
-                                        type="">Remover</button>
-                                </form>
-
+                                    <form method="POST" action="{{ route('categorias.destroy', $categoria) }}">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button class="w-20 rounded-md bg-red-600 px-2 hover:bg-red-900"
+                                            type="">Remover</button>
+                                    </form>
+                                </div>
                             </td>
                         </tr>
                     @endforeach

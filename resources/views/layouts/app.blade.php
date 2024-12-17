9541aab2b7b98f5ee3f566c7fb1a8e269e9464d5
kavvata <gabriel.kawata@gmail.com>
2024-12-17 16:33:03 -0300

    Correção de fundo branco ao utilizar rolagem cinética

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     @stack('scripts')
 </head>
 
-<body class="font-sans antialiased">
+<body class="font-sans antialiased dark:bg-gray-900">
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
         @include('layouts.navigation')
 

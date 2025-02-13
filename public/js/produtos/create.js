14d1d0db4951aa90a5f4c36603d0a8a410ffc733
kavvata <gabriel.kawata@gmail.com>
2025-02-13 12:53:17 -0300

    Implementado novo input de quantidade de produto

--- public/js/produtos/create.js
+++ public/js/produtos/create.js
@@ -28,7 +28,32 @@ const previewImage = (event) => {
     }
 }
 
+const diminuirQuantidade = (event) => {
+    let quantidadeInput = document.getElementById("quantidade")
+
+
+    quantidadeInput.value = Number(quantidadeInput.value) - 1
+    validarQuantidade(event)
+}
+const aumentarQuantidade = (event) => {
+    let quantidadeInput = document.getElementById("quantidade")
+
+    quantidadeInput.value = Number(quantidadeInput.value) + 1
+    validarQuantidade(event)
+}
+
+const validarQuantidade = (event) => {
+    let quantidadeInput = event.target.id == "quantidade" ? event.target : document.getElementById("quantidade")
+
+    if (quantidadeInput.value < 1) {
+        quantidadeInput.value = 1
+    }
+}
+
 document.addEventListener('edit-form-loaded', function () {
+    document.getElementById("increment-button").addEventListener("click", aumentarQuantidade);
+    document.getElementById("decrement-button").addEventListener("click", diminuirQuantidade);
+    document.getElementById("quantidade").addEventListener("change", validarQuantidade);
     document.getElementById("preco").addEventListener("keyup", formatar);
     document.getElementById('imageUpload').addEventListener('change', previewImage);
 })
--- resources/views/livewire/produtos/edit-form.blade.php
+++ resources/views/livewire/produtos/edit-form.blade.php
@@ -59,10 +59,33 @@
             </div>
 
             <div class="flex gap-3">
-                <input name="quantidade" type="text" placeholder="Quantidade" value="{{ $produto->quantidade }}"
-                    class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600">
+                <div class="relative flex items-center">
+                    <button type="button" id="decrement-button"
+                        class="bg-gray-100 dark:bg-gray-900 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-md p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                        <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M1 1h16" />
+                        </svg>
+                    </button>
+                    <input type="text" id="quantidade" data-input-counter aria-describedby="helper-text-explanation"
+                        class="bg-gray-50 border-x-0 border-gray-300 h-11 font-medium text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full pb-6 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="" value="{{ $produto->quantidade ?? 1 }}" required />
+                    <div
+                        class="absolute bottom-1 start-1/2 -translate-x-1/2 rtl:translate-x-1/2 flex items-center text-xs text-gray-400 space-x-1 rtl:space-x-reverse">
+                        <span>Qntd.</span>
+                    </div>
+                    <button type="button" id="increment-button"
+                        class="bg-gray-100 dark:bg-gray-900 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-md p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                        <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M9 1v16M1 9h16" />
+                        </svg>
+                    </button>
+                </div>
                 <select name="unidade_medida_id"
-                    class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600">
+                    class="w-1/3 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600">
                     @foreach ($unidadeMedidas as $unidadeMedida)
                         <option @if ($unidadeMedida == $produto->unidadeMedida) selected="selected" @endif
                             value="{{ $unidadeMedida->id }}">
